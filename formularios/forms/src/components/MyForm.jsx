8628f80d2cde6b023775f42becf8f9730fbd0b42
malinhodev <malinhodev@gmail.com>
2024-02-19 20:06:45 -0300

    aprendendo a usar onSubmit e onchange nos inputs

--- formularios/forms/src/components/MyForm.jsx
+++ formularios/forms/src/components/MyForm.jsx
@@ -1,14 +1,30 @@
+import { useState } from 'react'
 import './MyForm.css'
 const MyForm = () => {
+    const [name,setName] = useState()
+    const [email,setEmail] = useState()
+
+    const handleName = (e)=>{
+        setName(e.target.value)
+    }
+
+    
+
+    const handleSubmit = (event)=>{
+        event.preventDefault()
+        console.log('enviando formulário')
+        console.log(name,email)
+    }
+
   return (
     <div>
-        <form action="">
+        <form onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="name">Nome:</label>
-                <input type="text" name="name" id="" placeholder='digite seu nome.'/>
+                <input type="text" name="name" id="" placeholder='digite seu nome.' onChange={handleName}/>
                 <label htmlFor="">
                     <span>E-mail:</span>
-                    <input type="email" name="email" id="" placeholder='digite seu email.'/>
+                    <input type="email" name="email" id="" placeholder='digite seu email.' onChange={(e)=> setEmail(e.target.value)}/>
                 </label>
                 <input type="submit" value="enviar" />
             </div>
