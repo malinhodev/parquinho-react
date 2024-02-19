00711f5a20d773548e12bfb91409f40f03e11cf0
malinhodev <malinhodev@gmail.com>
2024-02-19 17:06:44 -0300

    utilizando label involto do input

--- formularios/forms/src/components/MyForm.jsx
+++ formularios/forms/src/components/MyForm.jsx
@@ -6,6 +6,10 @@ const MyForm = () => {
             <div>
                 <label htmlFor="name">Nome:</label>
                 <input type="text" name="name" id="" placeholder='digite seu nome.'/>
+                <label htmlFor="">
+                    <span>E-mail:</span>
+                    <input type="email" name="email" id="" placeholder='digite seu email.'/>
+                </label>
                 <input type="submit" value="enviar" />
             </div>
         </form>
