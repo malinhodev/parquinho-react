181b36c2834fd82a7d583bf9b1d251a765f594cf
malinhodev <malinhodev@gmail.com>
2024-02-19 20:51:51 -0300

    aprendendo a resgatar dados de uma option

--- formularios/forms/src/App.js
+++ formularios/forms/src/App.js
@@ -8,7 +8,8 @@ function App() {
         <h2>forms</h2>
         <MyForm user={{
           name: "Maicon",
-          email: "maicon@email.com"
+          email: "maicon@email.com",
+          bio: ''
         }}/>
     </div>
   );
--- formularios/forms/src/components/MyForm.jsx
+++ formularios/forms/src/components/MyForm.jsx
@@ -3,6 +3,8 @@ import './MyForm.css'
 const MyForm = ({user}) => {
     const [name,setName] = useState(user ? user.name : '')
     const [email,setEmail] = useState(user ? user.email : "")
+    const [bio,setBio] = useState(user ? user.bio : "")
+    const [cargo,setCargo] = useState('')
 
     const handleName = (e)=>{
         setName(e.target.value)
@@ -13,10 +15,12 @@ const MyForm = ({user}) => {
     const handleSubmit = (event)=>{
         event.preventDefault()
         console.log('enviando formulário')
-        console.log(name,email)
+        console.log(name,email,bio,cargo)
         //clear form 
         setName('')
         setEmail('')
+        setBio('')
+        setCargo('')
     }
 
   return (
@@ -28,6 +32,18 @@ const MyForm = ({user}) => {
                 <label htmlFor="">
                     <span>E-mail:</span>
                     <input type="email" name="email" id="" placeholder='digite seu email.' onChange={(e)=> setEmail(e.target.value)} value={email}/>
+                </label>
+                <label htmlFor="">
+                    <span>Bio:</span>
+                    <textarea name="bio" onChange={(e)=> setBio(e.target.value)} value={bio} placeholder='digite sua biografia'></textarea>
+                </label>
+                <label htmlFor="">
+                    <span>cargo:</span>
+                    <select name="cargo" onChange={(e)=> setCargo(e.target.value)}>
+                        <option value="editor">editor</option>
+                        <option value="chefe">chefe</option>
+                        <option value="fotografo">fotográfo</option>
+                    </select>
                 </label>
                 <input type="submit" value="enviar" />
             </div>
